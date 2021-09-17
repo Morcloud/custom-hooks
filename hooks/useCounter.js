864a3bcb8e071e9a6fab06fef8f29051ef718d20
Ricardo Tornero <richardo@outlook.com>
2021-09-16 19:28:14 -0500

    primeros hooks

---
+++ hooks/useCounter.js
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+export const useCounter = ( initialState = 10 ) => {
+    
+    const [counter, setCounter] = useState(initialState)
+
+    const increment = ( factor = 1) => setCounter( counter + factor )
+
+    const decrement = ( factor = 1 ) => setCounter( counter - factor )
+
+    const reset = () => setCounter( initialState )
+
+    return {
+        counter,
+        increment,
+        decrement,
+        reset 
+    }
+
+}
